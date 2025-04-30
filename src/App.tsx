62645bcf64bc4a7483ff912affc4fd1132f2e265
spanzy teo <spanzytheo@gmail.com>
2025-04-30 12:26:50 +0100

    initialize stai landing page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./Components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <div className="px-[1.5rem] md:px-[6.5rem] pt-[1.5rem]">
+        <Navbar />
+      </div>
+    </>
+  )
+}
+
+export default App
