8cc868472d29a26c3f6f706b80da24381939dd76
spanzy teo <spanzytheo@gmail.com>
2025-05-06 08:30:06 +0100

    modification on banner section

--- src/Components/Home/Banner.tsx
+++ src/Components/Home/Banner.tsx
@@ -1,3 +1,5 @@
+import { motion, useInView, useAnimation } from 'framer-motion'
+
 const Banner = () => {
   return (
     <section className="relative text-center px-[1.5rem] md:px-[6.5rem]">
@@ -11,12 +13,12 @@ const Banner = () => {
 
       {/* Content */}
       <div className="relative z-10">
-        <h1>
-          <span className="bg-gradient-to-r from-blue-500 via-pink-500 to-orange-400 bg-clip-text text-transparent playfair text-[2.375rem] md:text-[3.75rem] font-bold">
+        <h1 className="playfair font-bold">
+          <span className="bg-gradient-to-r from-blue-500 via-pink-500 to-orange-400 bg-clip-text text-transparent text-[2.375rem] md:text-[3.75rem] ">
             Your #1 AI Voice First
           </span>
           <br />
-          <span className="text-gray-700 playfair text-[2.375rem] md:text-[3.75rem] font-bold">
+          <span className="text-gray-700 text-[2.375rem] md:text-[3.75rem]">
             Control App for Productivity
           </span>
         </h1>
@@ -26,20 +28,24 @@ const Banner = () => {
         </p>
 
         {/* Input Box */}
-        <div className="mt-[1.25rem] md:mt-[4.1rem] flex justify-center">
-          <div className="p-[2px] rounded-[1rem] bg-gradient-to-r from-[#8E77FF] via-[#F0DBE6] via-[#FFDEB4] via-[#B1E575] via-[#BEF4CF] to-[#89B9F7]">
+        <div className="mt-[1.25rem] md:mt-[4.1rem] flex w-full md:w-[31.7rem] md:h-[4.5rem] mx-auto p-1 bg-gradient-to-r from-[#8E77FF] via-[#F0DBE6] via-[#FFDEB4] via-[#B1E575] via-[#BEF4CF] to-[#89B9F7] rounded-[0.31rem] md:rounded-[1rem]">
+          <div className="flex w-full bg-white rounded-[0.31rem] md:rounded-[1rem] p-1">
             <input
               type="email"
               placeholder="Enter your personal or work email"
-              className="px-[0.2rem] py-[0.2rem] md:p-[0.75rem] md:inline-flex flex items-center rounded-[0.31rem] md:rounded-[0.95rem] md:gap-[0.25rem] border-none outline-none w-[10rem] md:w-[31.5rem] md:h-[4.5rem] h-[1.4rem] gap-[0.08rem] bg-white"
+              className="flex-grow md:px-3 px-[0.23rem] py-2 text-sm text-gray-700 placeholder-gray-400 bg-transparent focus:outline-none rounded-full"
             />
+            <button className="md:px-7 px-3 md:py-3 text-sm text-white bg-gray-900 md:rounded-[0.75rem] rounded-[0.226rem] hover:bg-gray-800 transition cursor-pointer dm-sans leading-[0.47rem] md:leading-[1.5rem] text-[0.6rem] md:text-[1rem] whitespace-nowrap">
+              Join Waitlist
+            </button>
           </div>
-
-          {/* <button className="bg-gray-900 text-white px-6 py-3 rounded-r-lg">
-            Join Waitlist
-          </button> */}
         </div>
-        <img src="/mockups.png" alt="mockup" className="mt-10" />
+
+        <img
+          src="/mockups.png"
+          alt="mockup"
+          className="mt-[1.5rem] md:mt-[3.69rem] mx-auto"
+        />
       </div>
     </section>
   )
